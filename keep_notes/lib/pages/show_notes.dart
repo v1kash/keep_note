c067188af1b0f72011c8543612b7acace0e20f68
Vikash <ivikash.thakur1@gmail.com>
2024-10-20 18:31:59 +0900

    Crated show notes page

--- keep_notes/lib/pages/show_notes.dart
+++ keep_notes/lib/pages/show_notes.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class ShowNotes extends StatefulWidget {
+  const ShowNotes({super.key});
+
+  @override
+  State<ShowNotes> createState() => _ShowNotesState();
+}
+
+class _ShowNotesState extends State<ShowNotes> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: [
+          Text("First note"),
+          SizedBox(height: 32,),
+          Text("second note"),
+        ],
+            );
+  }
+}
